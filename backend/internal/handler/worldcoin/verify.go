73739c7d636a82d2bd382396143e1cd2d48cdd77
Thawinwit <a667375@AR8040667375N1.local>
2024-09-21 20:55:34 +0700

    finish setting up http + inject from main

--- backend/internal/handler/worldcoin/verify.go
+++ backend/internal/handler/worldcoin/verify.go
@@ -1,8 +1,10 @@
 package worldcoin
 
 import (
+	"net/http"
+
 	"github.com/go-playground/validator/v10"
-	"github.com/thachawit/factify/internal/core/model"
+	"github.com/labstack/echo/v4"
 	"github.com/thachawit/factify/internal/core/port/inbound"
 )
 
@@ -11,13 +13,13 @@ type worldCoinHandler struct {
 	worldCoinService inbound.WorldCoinInterface
 }
 
-func NewWorldCoinHandler(validate validator.Validate, worldCoinService inbound.WorldCoinInterface) *worldCoinHandler {
+func NewWorldCoinHandler(validate *validator.Validate, worldCoinService inbound.WorldCoinInterface) *worldCoinHandler {
 	return &worldCoinHandler{
-		validate:         &validate,
+		validate:         validate,
 		worldCoinService: worldCoinService,
 	}
 }
 
-func (h *worldCoinHandler) VerifyProof(proofRequest model.WorldCoinHandlerRequest) (*model.WorldCoinHandlerResponse, error) {
-	return &model.WorldCoinHandlerResponse{}, nil
+func (h *worldCoinHandler) VerifyProof(c echo.Context) error {
+	return c.JSON(http.StatusOK, "")
 }
